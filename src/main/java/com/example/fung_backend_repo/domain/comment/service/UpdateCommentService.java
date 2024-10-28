c103042b94c979b7ede428ca0c1e8aa82620f71b
sorim <enhld00@gmail.com>
2024-10-28 14:23:57 +0900

    📝 :: import 수정

--- src/main/java/com/example/fung_backend_repo/domain/comment/service/UpdateCommentService.java
+++ src/main/java/com/example/fung_backend_repo/domain/comment/service/UpdateCommentService.java
@@ -1,11 +1,9 @@
 package com.example.fung_backend_repo.domain.comment.service;
 
-import com.example.fung_backend_repo.domain.comment.CommentFacade;
+import com.example.fung_backend_repo.domain.comment.facade.CommentFacade;
 import com.example.fung_backend_repo.domain.comment.domain.Comment;
-import com.example.fung_backend_repo.domain.comment.domain.repository.CommentRepository;
 import com.example.fung_backend_repo.domain.comment.exception.CommentNotFoundException;
 import com.example.fung_backend_repo.domain.comment.presentation.request.UpdateCommentRequest;
-import com.example.fung_backend_repo.domain.feed.facade.FeedFacade;
 import com.example.fung_backend_repo.domain.user.domain.User;
 import com.example.fung_backend_repo.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UpdateCommentService {
 
-    private final CommentRepository commentRepository;
     private final UserFacade userFacade;
     private final CommentFacade commentFacade;
 
